c9738b0dcffe3ab721d2304299cf4d0b4b6e1e44
ivanbezniak <ivan.bezniak@gmail.com>
2025-01-15 00:05:00 +0300

    Fix: added app keys

--- config/plugins.js
+++ config/plugins.js
@@ -1,11 +1,20 @@
+// module.exports = ({ env }) => ({
+//     // ...
+//     "users-permissions": {
+//         config: {
+//             register: {
+//                 allowedFields: ["userphone"],
+//             },
+//         },
+//     },
+//     // ...
+// });
+
+
 module.exports = ({ env }) => ({
-    // ...
     "users-permissions": {
         config: {
-            register: {
-                allowedFields: ["userphone"],
-            },
+            jwtSecret: env('JWT_SECRET'),
         },
     },
-    // ...
 });
