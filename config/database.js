1fef1969f0aa62b838df812cad9765f96f98ebf9
ivanbezniak <ivan.bezniak@gmail.com>
2025-01-14 21:47:03 +0300

    Fix engines for Heroku

--- config/database.js
+++ config/database.js
@@ -67,14 +67,15 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'your_database_name'),
-      user: env('DATABASE_USERNAME', 'your_username'),
-      password: env('DATABASE_PASSWORD', 'your_password'),
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
       ssl: {
         rejectUnauthorized: false,
       },
     },
   },
 });
+
