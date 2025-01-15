5f47fd080e74c89c90d552fb9fef492ff65f912f
ivanbezniak <ivan.bezniak@gmail.com>
2025-01-15 22:03:11 +0300

    Fix: added app keys444

--- config/admin.js
+++ config/admin.js
@@ -14,4 +14,8 @@ module.exports = ({ env }) => ({
     nps: env.bool('FLAG_NPS', true),
     promoteEE: env.bool('FLAG_PROMOTE_EE', true),
   },
+  server: {
+    host: env('HOST', '0.0.0.0'),  // Используйте этот хост
+    port: env.int('PORT', 1337),   // Порт, который вы используете
+  },
 });
